257c57c56e1f1b694c4030c055df44f881a095b0
peawza <56231020+peawza@users.noreply.github.com>
2020-09-26 14:01:47 +0700

    1
    1122

---
+++ php/updatepassword.php
@@ -0,0 +1,44 @@
+<?php
+//session_start();
+require_once('connect.php');
+echo $_SESSION['ID'];
+print_r($_POST);
+$username = $_SESSION['Username'];
+$password = $_POST['password'];
+$hashed_password = password_hash($_POST['password1'], PASSWORD_DEFAULT);
+echo $hashed_password;
+$sqlUpdate = "UPDATE `user` SET `User_Password` = '" . $hashed_password . "'  WHERE `User_ID` = '" . $_SESSION['ID'] . "' ";
+
+//UPDATE `user` SET `User_Password` = '5' WHERE `user`.`User_ID` = 10;
+//$sevasql = $conn->query($sqlupdatephoto) or die($conn->error);
+
+if (isset($_POST['submitUpdatepassword']) && isset($_SESSION['ID'])) {
+    //echo 'มี';
+    $sql = "SELECT * FROM `user` WHERE `User_ID`= '" . $_SESSION["ID"] . "'"; // alt  + 96
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    print_r($row);
+
+    if (password_verify($password, $row['User_Password'])) {
+
+
+
+
+        $result = $conn->query($sqlUpdate) or die($conn->error);
+
+        if ($result) {
+            //echo 'แก้ไข้ข้อมูลสำเร็จ';
+            echo "<script> alert('แก้ไขข้อมูลสำเร็จ'); </script>";
+            header('location:../index.php');
+        } else {
+            //echo 'ไม่สำเร็จ';
+            echo "<script> alert('แก้ไขข้อมูลไม่สำเร็จ โปรดติดต่อเจ้าหน้าที่'); </script>";
+            header('location:../index.php');
+        }
+    } else {
+        echo "<script> alert('รหัสผ่านไม่ตรงกับอันเก่า'); </script>";
+        header('Refresh:0; url=../prodile_editpassword.php');
+    }
+} else {
+    header('location:../index.php');
+}
